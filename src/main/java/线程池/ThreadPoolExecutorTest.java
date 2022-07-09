c2856a531e9c8fa0307ea5919804e9c39cd8b14c
berior <1269921156@qq.com>
2022-07-09 21:14:54 +0800

    Java Study

---
+++ src/main/java/线程池/ThreadPoolExecutorTest.java
@@ -0,0 +1,6 @@
+package 线程池;
+
+public class ThreadPoolExecutorTest {
+    public static void main(String[] args){
+    }
+}
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,18 +1,20 @@
-import java.util.concurrent.ThreadLocalRandom;
-
 public class Test {
     public static void main(String[] args) {
-        ThreadLocalRandom random = ThreadLocalRandom.current();
-        int count = 0;
-        for (int i = 0; i < 5; i++) {
-            if (isInterested(random.nextInt(10))) {
-                count++;
+        int[] arr = new int[43];
+
+        int cnt = 41;
+        int flag = 1;
+        while (cnt != 2) {
+            for (int i = 1; i < arr.length; i++) {
+                flag = flag % 3;
+                if (arr[i] == 1) {
+                    i++;
+                    flag += 1;
+                }
+                if (flag == 3) {
+
+                }
             }
         }
-        System.out.printf("Found %d interested values%n", count);
-    }
-
-    private static boolean isInterested(int i) {
-        return i % 2 == 0;
     }
 }
