561902e37d0598a22954a8214fb459eb2d1ab6ef
berior <1269921156@qq.com>
2022-07-10 22:47:05 +0800

    Java Study

---
+++ java-redis/src/test/java/io/paches/javaredis/JavaRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package io.paches.javaredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
