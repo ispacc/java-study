e6d9dbc974885162a3421f80bd3794988c86b5d5
berior <1269921156@qq.com>
2022-07-21 00:25:09 +0800

    update1

--- java-redis/src/main/java/io/paches/javaredis/config/RedissonConfig.java
+++ java-redis/src/main/java/io/paches/javaredis/config/RedissonConfig.java
@@ -4,6 +4,7 @@ public class RedissonConfig {
     public static void main(String[] args) {
         System.out.println("HeeloWorld");
         System.out.println();
+        System.out.println();
 
     }
 }
