9ebec7799661622c2b0c585b737d99f193eeb704
十四 <yh.xion@outlook.com>
2022-11-28 18:00:06 +0800

    update

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,13 +1,5 @@
-import java.io.File;
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        List list = Arrays.asList("a", "b", "c");
-        list.forEach(System.out::println);
 
-        var files = new File(".").listFiles(File::isHidden);
-        
     }
 }
--- src/test/java/java_8_in_action/ParallelStreamTest.java
+++ src/test/java/java_8_in_action/ParallelStreamTest.java
@@ -1,12 +1,23 @@
 package java_8_in_action;
 
 import java.util.function.Function;
+import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
 public class ParallelStreamTest {
     public static void main(String[] args) {
-        System.out.println("Sequential sum done in:"+
-                measureSumPerf(ParallelStreamTest::parallelsSum, 10_000_000)+" msecs");
+        System.out.println("Sequential sum done in:" +
+                measureSumPerf(ParallelStreamTest::sequentialSum, 10_000_000) + " msecs");
+
+        System.out.println("parallelsSum sum done in:" +
+                measureSumPerf(ParallelStreamTest::parallelsSum, 10_000_000) + " msecs");
+
+
+        System.out.println("rangedSum sum done in:" +
+                measureSumPerf(ParallelStreamTest::rangedSum, 10_000_000) + " msecs");
+
+        System.out.println("parallelRangedSum sum done in:" +
+                measureSumPerf(ParallelStreamTest::parallelRangedSum, 10_000_000) + " msecs");
     }
 
     public static long sequentialSum(long n) {
@@ -23,14 +34,26 @@ public class ParallelStreamTest {
     }
 
     public static long measureSumPerf(Function<Long, Long> adder, long n) {
-        long fastest=Long.MAX_VALUE;
-        for (int i=0; i < 10; i++) {
-            long start=System.nanoTime();
-            long sum=adder.apply(n);
-            long duration=(System.nanoTime() - start) / 1_000_000;
-            System.out.println("Result: "+sum);
-            if (duration < fastest) fastest=duration;
+        long fastest = Long.MAX_VALUE;
+        for (int i = 0; i < 10; i++) {
+            long start = System.nanoTime();
+            long sum = adder.apply(n);
+            long duration = (System.nanoTime() - start) / 1_000_000;
+//            System.out.println("Result: "+sum);
+            if (duration < fastest) fastest = duration;
         }
         return fastest;
     }
+
+    public static long rangedSum(long n) {
+        return LongStream.rangeClosed(1, n)
+                .reduce(0L, Long::sum);
+    }
+
+    public static long parallelRangedSum(long n) {
+        return LongStream.rangeClosed(1, n)
+                .parallel()
+                .reduce(0L, Long::sum);
+    }
+
 }
