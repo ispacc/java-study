cf451648e8f38757f22f492f62c0eaa0b68c62f5
十四 <yh.xion@outlook.com>
2023-03-22 09:13:17 +0800

    update

--- src/main/java/io/Person.java
+++ src/main/java/io/Person.java
@@ -1,10 +1,8 @@
 package io;
 
-public class Person{
+public class Person {
     static int[] arr = new int[10];
+
     public static void main(String[] arg) {
-        short s1 = 1;
-        s1 += 1;
-        System.out.println(s1);
     }
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,4 +1,10 @@
+import java.util.Scanner;
+
 public class Test {
-
-
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            String next = in.next();
+            System.out.println(next);
+        }
+    }
 }
