eaa69ab01cf9f8f9b6a1a6a5914c83ffc3ccd675
十四 <yh.xion@outlook.com>
2023-04-06 11:32:06 +0800

    1

--- src/main/java/线程池/ThreadPoolExecutorTest.java
+++ src/main/java/线程池/ThreadPoolExecutorTest.java
@@ -1,13 +1,14 @@
 package 线程池;
 
 public class ThreadPoolExecutorTest {
-    public static void main(String[] args){
-        new Thread( () -> System.out.println("Hello World")) {
+    public static void main(String[] args) {
+        new Thread(() -> System.out.println("Hello World")) {
             @Override
             public void run() {
                 System.out.println("zi run()");
             }
         }.start();
+
     }
 
 }
